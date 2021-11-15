ee116f2248e8ff2bfc628c742bdaee92a45bb687
Amandeep-98 <w0775616@myscc.ca>
2021-11-15 09:45:20 -0500

    Add project files.

---
+++ KaurBooks.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using KaurBooks.DataAccess.Migrations;
+using KaurBooks.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AmandeepKBookStore.DataAccess.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ KaurBooks.DataAccess/Migrations/Repository/UnitOfWork.cs
@@ -0,0 +1,37 @@
+﻿using KaurBooks.DataAccess.Repository.IRepository;
+using AmandeepKBookStore.DataAccess.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KaurBooks.DataAccess.Repository
+{
+    public class UnitOfWork : IUnitOfWork      //make the method public to access the class 
+    {
+        private readonly ApplicationDbContext _db;   // the using statement 
+
+        public UnitOfWork(ApplicationDbContext db)   // constructor to use DI and inject in the repository
+        {
+            _db = db;
+            Category = new CategoryRepository(_db);
+            SP_Call = new SP_Call(_db);
+        }
+
+        public ICategoryRepository Category { get; private set; }
+
+        public ISP_Call SP_Call { get; private set; }
+
+        public void Dispose()
+        {
+            _db.Dispose();
+        }
+
+        public void Save()   // all changes will be saved when the save method is called at parent level.
+        {
+            _db.SaveChanges();
+        }
+
+    }
+}

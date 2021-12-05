8d7c6af20afb9b5dd2b440d27f335203994c9c98
Amandeep-98 <w0775616@myscc.ca>
2021-12-04 21:17:55 -0500

    Changed

--- KaurBooks.DataAccess/Migrations/Repository/IRepository/IUnitOfWork.cs
+++ KaurBooks.DataAccess/Migrations/Repository/IRepository/IUnitOfWork.cs
@@ -1,19 +1,19 @@
-﻿using KaurBooks.DataAccess.Migrations.Repository.IRepository;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
 
 namespace KaurBooks.DataAccess.Repository.IRepository
 {
     public interface IUnitOfWork : IDisposable
     {
         ICategoryRepository Category { get; }
+<<<<<<< HEAD
         ICoverTypeRepository CoverType { get; }
         IProductRepository Product { get; }
         ISP_Call SP_Call { get; }
+=======
+
+        ISP_Call SP_Call { get; }
+        
+>>>>>>> d4ee44d8d860d6acd668629e1be7d4fc91fe9837
 
         void Save();
     }
